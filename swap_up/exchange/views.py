3407aa5f7057635f9a2564bd09f31c29ec359841
magdalena <magdexxa@gmail.com>
2021-03-24 19:46:07 +0100

    Add home page

--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# from django.template import loader
+from django.views.generic import View
+from django.views import generic
+
 
 # Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = 'exchange/index.html'
--- swap_up/swap_up/urls.py
+++ swap_up/swap_up/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from exchange import views as exchange_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', exchange_views.IndexView.as_view(), name='index'),
+    ]
