7d6f7c17b82af35129d4abf16031164e13b66d44
Aleksandra Cynk <305284@student.agh.edu.pl>
2021-04-07 14:17:27 +0200

    Upload a file

--- swap_up/exchange/urls.py
+++ swap_up/exchange/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='user-home')
+    path('', views.home, name='user-home'),
+    path('upload_shedule', views.upload_shedule, name='upload-shedule')
 ]
--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 # from django.template import loader
 from django.views.generic import View
 from django.views import generic
+from django.utils import timezone
+
+from django.http import HttpResponseRedirect
+
+
+from .forms import UploadFileForm
+
+
+# Imaginary function to handle an uploaded file.
+"""from somewhere import handle_uploaded_file"""
 
 
 # Create your views here.
@@ -13,6 +23,26 @@ class IndexView(generic.TemplateView):
 # zmienić potem path na odpowiedni plik html
 def home(request):
     return render(request, 'exchange/index.html')
+
+
+def upload_shedule(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        print(form.is_valid())
+        if form.is_valid():
+            #handle_uploaded_file(request.FILES['file'])
+            """
+            def handle_uploaded_file(f):
+                #przykładowa funkcja handle_uploaded_file
+                with open('some/file/name.txt', 'wb+') as destination:
+                    for chunk in f.chunks():
+                        destination.write(chunk)
+            """
+
+            return HttpResponseRedirect('/exchange/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'exchange/upload_shedule.html', {'form': form})
 
 def register():
     return render(request, 'exchange/index.html')
