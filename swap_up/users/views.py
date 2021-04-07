d30308b7b4f217b3b9dc7e0b42ed9aaad260b18a
wsrtka <witold.serwatka@pm.me>
2021-04-07 12:16:55 +0200

    add conditional rendering

--- swap_up/users/views.py
+++ swap_up/users/views.py
@@ -16,3 +16,7 @@ def register(request):
         form = UserRegisterForm()
     
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
