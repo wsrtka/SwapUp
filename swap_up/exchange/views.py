4da8fbae67c463a27feeabacbe174d6fb245ad70
wsrtka <witold.serwatka@pm.me>
2021-04-08 16:32:28 +0200

    set links

--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -14,35 +14,30 @@ from django.http import HttpResponseRedirect
 """from somewhere import handle_uploaded_file"""
 
 
-# Create your views here.
-
 class IndexView(generic.TemplateView):
     template_name = 'exchange/index.html'
 
 def home(request):
     return render(request, 'exchange/index.html')
 
-def upload_shedule(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        print(form.is_valid())
-        if form.is_valid():
-            #handle_uploaded_file(request.FILES['file'])
-            """
-            def handle_uploaded_file(f):
-                #przykładowa funkcja handle_uploaded_file
-                with open('some/file/name.txt', 'wb+') as destination:
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-            """
+# def upload_shedule(request):
+#     if request.method == 'POST':
+#         form = UploadFileForm(request.POST, request.FILES)
+#         print(form.is_valid())
+#         if form.is_valid():
+#             #handle_uploaded_file(request.FILES['file'])
+#             """
+#             def handle_uploaded_file(f):
+#                 #przykładowa funkcja handle_uploaded_file
+#                 with open('some/file/name.txt', 'wb+') as destination:
+#                     for chunk in f.chunks():
+#                         destination.write(chunk)
+#             """
 
-            return HttpResponseRedirect('/exchange/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'exchange/upload_shedule.html', {'form': form})
-
-def register():
-    return render(request, 'exchange/index.html')
+#             return HttpResponseRedirect('/exchange/')
+#     else:
+#         form = UploadFileForm()
+#     return render(request, 'exchange/upload_shedule.html', {'form': form})
 
 def offers(request):
     return render(request, 'exchange/offers.html')
