ad08df8f52b475345c87d8a0320ff4ef18270780
magdalena <magdexxa@gmail.com>
2021-04-10 16:47:48 +0200

    Change import_schedule to handle file arg & create classes and add to list_of_classes of current student

--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -25,27 +25,25 @@ def home(request):
 def import_schedule(csv_file):
     
     classes = []
-    csv_file = request.FILES[filename]
+    student = request.user
    
-    with open(csv_file, newline='\n') as csv_file:
-        reader = csv.reader(csv_file, delimiter = ';', quotechar = '|')
-        for row in reader:
-            # print(len(row))
-            subject_name = column[0]
-            term_type = column[1]
-            term_capacity = column[2]
-            group_number = column[3]
-            teacher_name = column[4]
-            room = column[5]
-            week = column[6] 
-            day = column[7]
-            hour = column[8]
+    reader = csv.reader(csv_file, delimiter = ';', quotechar = '|')
+    for row in reader:
 
-            subject = get_subject_by_name(subject_name)
-            teacher = get_teacher_by_name(teacher_name)
+        subject_name = column[0]
+        term_type = column[1]
+        term_capacity = column[2]
+        group_number = column[3]
+        teacher_name = column[4]
+        room = column[5]
+        week = column[6] 
+        day = column[7]
+        hour = column[8]
 
-        
-        created_class = Class(
+        subject = get_subject_by_name(subject_name)
+        teacher = get_teacher_by_name(teacher_name)
+
+        created_class = Class.objects.create(
             subject_id = subject,
             day = day,
             time = time,
@@ -55,6 +53,10 @@ def import_schedule(csv_file):
 
         classes.append(created_class)
 
+    for c in classes:
+        student.list_of_classes.add(c)
+
+
 
 def upload_shedule(request):
     if request.method == 'POST':
@@ -62,15 +64,6 @@ def upload_shedule(request):
         print(form.is_valid())
         if form.is_valid():
             import_schedule(request.FILES['file'])
-            # handle_uploaded_file(request.FILES['file'])
-            """
-            def handle_uploaded_file(f):
-                #przykładowa funkcja handle_uploaded_file
-                with open('some/file/name.txt', 'wb+') as destination:
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-            """
-
             return HttpResponseRedirect('/exchange/')
     else:
         form = UploadFileForm()
