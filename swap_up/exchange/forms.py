92bb36060f35aad7bb6a19a2948a990b7e66105f
Aleksandra Cynk <305284@student.agh.edu.pl>
2021-04-11 17:38:34 +0200

    Frontend improved

--- swap_up/exchange/forms.py
+++ swap_up/exchange/forms.py
@@ -8,7 +8,8 @@ DAY_OF_THE_WEEK_CHOICES = [('MON', 'Monday'), ('TUE','Tuesday'), ('WED','Wednesd
 TIME_CHOICES = [('8-00', '8:30 - 9:30'), ('9-35', '9:35 - 11:05'), ('11-15', '11:15 - 12:45'),
  ('12-50', '12:50 - 14:20'), ('14-40', '14:40 - 16:10'), ('16-15', '16:15 - 17:45'), ('17-50', '17:50 - 19:20')]
 
-
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
 
 class AddExchangeForm(forms.Form):
     subject_name = forms.CharField(label='Subject', max_length=100, required=True)
--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.views import generic
 from django.utils import timezone
-
+import io
+import csv
 from django.http import HttpResponseRedirect
 
-from .forms import AddExchangeForm
+from .forms import AddExchangeForm, UploadFileForm
 
 
 from django.shortcuts import render
@@ -30,17 +31,20 @@ def import_schedule(csv_file):
 
     io_string = io.StringIO(data)
     # next(io_string)
-    for column in csv.reader(io_string, delimeter = ',', quotechar = "|"):
-        subject_name = column[0]
-        term_type = column[1]
-        term_capacity = column[2]
-        group_number = column[3]
-        teacher_name = column[4]
-        room = column[5]
-        # TODO: zajecia co tydzien nie maja tej kolumny, sprawdzac liczbe kolumn
-        week = column[6] 
-        day = column[7]
-        hour = column[8]
+    for column in csv.reader(io_string, delimiter = ',', quotechar = "|"):
+        try:
+            subject_name = column[0] 
+            term_type = column[1]
+            term_capacity = column[2]
+            group_number = column[3]
+            teacher_name = column[4]
+            room = column[5]
+            # TODO: zajecia co tydzien nie maja tej kolumny, sprawdzac liczbe kolumn
+            week = column[6] 
+            day = column[7]
+            hour = column[8]
+        except:
+            pass
 
         subject = get_subject_by_name(subject_name)
         teacher = get_teacher_by_name(teacher_name)
@@ -56,25 +60,21 @@ def import_schedule(csv_file):
 
         classes.append(created_class)
 
-def upload_shedule(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        print(form.is_valid())
-        if form.is_valid():
-            import_schedule(request.FILES['file'])
-            # handle_uploaded_file(request.FILES['file'])
-            """
-            def handle_uploaded_file(f):
-                #przykładowa funkcja handle_uploaded_file
-                with open('some/file/name.txt', 'wb+') as destination:
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-            """
 
-            return HttpResponseRedirect('/exchange/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'exchange/upload_shedule.html', {'form': form})
+def upload_csv(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+
+        myfile = request.FILES['myfile']
+        for line in myfile:
+            print(line)
+
+        import_schedule(request.FILES['myfile'])
+
+        return render(request, 'exchange/upload_csv.html')
+
+    return render(request, 'exchange/upload_csv.html')
+
+
 
 def register():
     return render(request, 'exchange/index.html')
