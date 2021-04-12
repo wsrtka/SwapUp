d3dbe2b60dbc753ef1631c103b1d5d532e077673
magdalena <magdexxa@gmail.com>
2021-04-12 23:57:45 +0200

    Delete depricated import_schedule function

--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -24,44 +24,6 @@ def home(request):
     return render(request, 'exchange/index.html')
 
 
-
-def import_schedule(csv_file, user):
-    
-    classes = []
-    student = Student.objects.get(user = user)
-   
-    with io.TextIOWrapper(csv_file, encoding='utf-8', newline='\n') as f:
-        reader = csv.reader(f, delimiter=';')
-        for row in reader:
-
-            subject_name = column[0]
-            term_type = column[1]
-            term_capacity = column[2]
-            group_number = column[3]
-            teacher_name = column[4]
-            room = column[5]
-            week = column[6] 
-            day = column[7]
-            hour = column[8]
-
-            subject = Subject.objects.get(subject_name = subject_name)
-            teacher_first_name, teacher_last_name = teacher_name.split()
-            teacher = Teacher.objects.get(first_name = teacher_first_name, last_name = teacher_last_name)
-
-            created_class = Class.objects.create(
-                subject_id = subject,
-                day = day,
-                time = time,
-                row = row,
-                teacher_id = teacher
-            )
-
-            classes.append(created_class)
-
-    for c in classes:
-        student.list_of_classes.add(c)
-
-
 def import_schedule_for_year(csv_file):
     
     semester = 0
