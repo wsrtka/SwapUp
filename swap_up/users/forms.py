943ecb84577493089a022cdaa73f9bed121ca2b9
wsrtka <witold.serwatka@pm.me>
2021-04-22 13:33:44 +0200

    add profile update function

--- swap_up/users/forms.py
+++ swap_up/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from exchange.models import Student
 
 
 class UserRegisterForm(UserCreationForm):
@@ -17,4 +18,28 @@ class UserRegisterForm(UserCreationForm):
             'email',
             'password1',
             'password2'
+        ]
+
+
+class UserUpdateForm(forms.ModelForm):
+    
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name'
+        ]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = [
+            'index_number',
+            'semester',
+            'path'
         ]
