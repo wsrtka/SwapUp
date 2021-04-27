e70c183c82a2244c4d2f46e847b6f7298bb5db29
Aleksandra Cynk <305284@student.agh.edu.pl>
2021-04-27 12:35:00 +0200

    Collision detector

--- swap_up/exchange/views.py
+++ swap_up/exchange/views.py
@@ -384,7 +384,6 @@ def schedule(request):
     current_user = request.user
     student = Student.objects.get(user=current_user)
 
-    # TODO czy na pewno takie tygodnie
     schedule = {
         'Pn': [], 'Wt': [], 'Śr': [], 'Czw': [], 'Pt': []
     }
@@ -394,15 +393,16 @@ def schedule(request):
     context = {}
 
     for c in student.list_of_classes.all():
-        print(c)
         class_dict = {}
 
         subject = c.subject_id
         teacher = c.teacher_id
 
+        #TODO
         # Tutaj jest wartość, którą buttony z planu zajęć przesyłają fo formularza
         # Powinno być coś w stylu class id
         class_dict['id'] = 'Passed_Value_' + str(c.day)
+
 
         class_dict['subject_name'] = str(subject.subject_name)
         class_dict['category'] = str(subject.category)
@@ -411,29 +411,69 @@ def schedule(request):
         class_dict['teacher'] = str(teacher.first_name) + " " + str(teacher.last_name)
         class_dict['room'] = str(c.room)
         class_dict['week'] = str(c.week)
-        print(c.time)
+        class_dict['week'] = str(c.week)
+
+
         hour_start, minute_start, seconds_start = str(c.time).split(':')
         hour_start, minute_start = int(hour_start), int(minute_start)
         hour_end, minute_end = hour_start, minute_start
-
         minute_end += 90
         while minute_end >= 60:
             hour_end += 1
             minute_end -= 60
 
+
+
         class_dict['time'] = str(hour_start) + ":" + "{:02d}".format(minute_start) + " - " + str(
             hour_end) + ":" + "{:02d}".format(minute_end)
 
-        class_dict['week'] = str(c.week)
+        # Template from 7:30 to 20:00 (13 h total)
 
-        # Template from 7:00 to 20:00 (13 h total)
-        time_from = 7
+        #arrange top and bottom position of div in display
+        time_from = 7.5
         time_to = 20
+
 
         class_dict['top'] = 100 * (hour_start + minute_start / 60 - time_from) / (time_to - time_from)
         class_dict['bottom'] = 100 * (time_to - hour_end - minute_end / 60) / (time_to - time_from)
 
+        #arrange left and right position based on number of collisions
+
+        class_dict['start'] = hour_start + minute_start/60
+        class_dict['end'] = hour_end + minute_end/60
+
+        class_dict['colliders'] = []
+        class_dict['collider_id'] = 0
+
+        for other_class in schedule[str(c.day)]:
+            if (class_dict['end'] >= other_class['start'] >= class_dict['start']) or ( class_dict['end'] >= other_class['end'] >= class_dict['start']) :
+
+                if other_class not in class_dict['colliders']:
+                    class_dict['colliders'].append(other_class)
+                if class_dict not in other_class['colliders']:
+                    other_class['colliders'].append(class_dict)
+
+                for other_class_collider in other_class['colliders']:
+                    if other_class_collider not in class_dict['colliders']:
+                        class_dict['colliders'].append(other_class_collider)
+                    if class_dict not in other_class_collider['colliders']:
+                        other_class_collider['colliders'].append(class_dict)
+
+                if class_dict in class_dict['colliders']:
+                    class_dict['colliders'].remove(class_dict)
+
+                class_dict['collider_id'] = len(class_dict['colliders'])
+
         schedule[str(c.day)].append(class_dict)
+
+    for day in ('Pn', 'Wt', 'Śr', 'Czw', 'Pt'):
+        for class_dict in schedule[day]:
+            n = len(class_dict['colliders']) + 1
+            i = class_dict['collider_id']
+
+            class_dict['width'] = 100/n
+            class_dict['left'] = (i) * (100/n)
+            print(class_dict['left'])
 
     context = [{'schedule': schedule['Pn'], 'name': 'Poniedziałek'},
                 {'schedule': schedule['Wt'], 'name': 'Wtorek'},
