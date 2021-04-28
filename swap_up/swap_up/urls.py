084a883ee9b4f614d75107ec8c1db995f6849874
wsrtka <witold.serwatka@pm.me>
2021-04-28 16:57:39 +0200

    fix commit

--- swap_up/swap_up/urls.py
+++ swap_up/swap_up/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
     path('profile/', user_views.profile, name='profile'),
     path('profile/update', user_views.update_profile, name='update-profile'),
     path('profile/delete', user_views.delete_profile, name='delete-profile')
