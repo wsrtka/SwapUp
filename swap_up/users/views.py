c091be388b4fc6da5f2c684a5d4c68960f9c60d5
magdalena <magdexxa@gmail.com>
2021-04-28 16:58:03 +0200

    Add index and semester fields in register

--- swap_up/users/views.py
+++ swap_up/users/views.py
@@ -11,8 +11,23 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
+
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.student.index_number = form.cleaned_data.get('index')
+            user.student.semester = form.cleaned_data.get('semester')
+            user.save()
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=user.username, password=raw_password)
+            # login(request, user)
+            # return redirect('home')
+
+
+
+
             username = form.cleaned_data.get('username')
+
             messages.success(request, f'Account created for {username}. You can now log in.')
             return redirect('login')
     else:
