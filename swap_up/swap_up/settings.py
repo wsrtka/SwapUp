fdb4b97d6fe002651b96be1189ee0fdbc6f68016
magdalena <magdexxa@gmail.com>
2021-05-26 00:56:24 +0200

    Change database engine to postgresql

--- swap_up/swap_up/settings.py
+++ swap_up/swap_up/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'swap_up.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
