7dd29fa272857bbc74407ce3e5f424c50af6e546
magdalena <magdexxa@gmail.com>
2021-05-26 02:03:13 +0200

    aaaa

--- swap_up/swap_up/wsgi.py
+++ swap_up/swap_up/wsgi.py
@@ -14,4 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swap_up.settings')
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swap_up.swap_up.settings')
 
+
 application = get_wsgi_application()
